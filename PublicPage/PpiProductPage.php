43e129edc102b45b28e0cb85401eaad6c274ffff
Jason Goossens <jason.goossens@gmail.com>
2021-04-14 12:26:58 +0200

    add language code to imaxel site url param

--- PublicPage/PpiProductPage.php
+++ PublicPage/PpiProductPage.php
@@ -467,11 +467,11 @@ class PpiProductPage
 			$status = 'error';
 			$information = $create_project_response['body'];
 		}
-		$siteUrl = get_site_url();
 		$backUrl =  explode("?", get_permalink($variant_id), 2)[0];
 		$encoded_response = json_decode($create_project_response['body']);
 		$project_id = $encoded_response->id;
 		$lang = isset($_COOKIE['wp-wpml_current_language']) && $_COOKIE['wp-wpml_current_language'] ? $_COOKIE['wp-wpml_current_language'] : 'en';
+		$siteUrl = get_site_url() . '/' . $lang;
 
 		$editorUrl = $imaxel->get_editor_url($project_id, $backUrl, $lang, $siteUrl . '/?add-to-cart=' . $variant_id . '&project=' . $project_id);
 
