ab40866515ec9a738062080a267df2f3e26cd514
Jason Goossens <jason.goossens@gmail.com>
2021-05-03 09:35:26 +0200

    added underscore to hide meta data from views

--- PublicPage/PpiProductPage.php
+++ PublicPage/PpiProductPage.php
@@ -517,7 +517,7 @@ class PpiProductPage
 		if (!isset($_GET['project'])) return $cart_item_data;
 
 		$projectId = esc_attr($_GET['project']);
-		$cart_item_data["ppi_imaxel_project_id"] = $projectId;
+		$cart_item_data["_ppi_imaxel_project_id"] = $projectId;
 
 		return $cart_item_data;
 	}
@@ -527,8 +527,8 @@ class PpiProductPage
 	 */
 	public function add_project_to_order_line_item($item, $cart_item_key, $values, $order)
 	{
-		if (isset($values['ppi_imaxel_project_id'])) {
-			$item->add_meta_data('ppi_imaxel_project_id', $values['ppi_imaxel_project_id'], true);
+		if (isset($values['_ppi_imaxel_project_id'])) {
+			$item->add_meta_data('_ppi_imaxel_project_id', $values['_ppi_imaxel_project_id'], true);
 		}
 	}
 
@@ -542,17 +542,19 @@ class PpiProductPage
 		$orderItems = $wc_order->get_items();
 		if (empty($orderItems)) return;
 
-		// loop over orderlines
 		foreach ($orderItems as $orderItemId => $orderItem) {
-			$imaxelProjectId = wc_get_order_item_meta($orderItemId, 'ppi_imaxel_project_id');
+			$imaxelProjectId = wc_get_order_item_meta($orderItemId, '_ppi_imaxel_project_id');
 			if (empty($imaxelProjectId)) continue;
 
 			$imaxel = new ImaxelService();
-			$imaxelResponse = $imaxel->create_order($imaxelProjectId, $orderId);
+			$imaxelResponse = $imaxel->create_order($imaxelProjectId, $orderId)['body'];
+			error_log(__FILE__ . ': ' . __LINE__ . ' ' . print_r($imaxelResponse, true) . PHP_EOL, 3, __DIR__ . '/currentOrderLog.txt');
 		}
 
 		$imaxel = new ImaxelService();
 		$pendingOrders = $imaxel->get_pending_orders();
-		error_log(__FILE__ . ': ' . __LINE__ . ' ' . print_r($pendingOrders['body'], true) . PHP_EOL, 3, __DIR__ . '/pendingOrdersLog.txt');
+		error_log(__FILE__ . ': ' . __LINE__ . ' ' . print_r($pendingOrders, true) . PHP_EOL, 3, __DIR__ . '/pendingOrdersLog.txt');
+		error_log(PHP_EOL, 3, __DIR__ . '/Log.txt');
+		// TODO make cronjob or something similar that get pending orders and downloads those!!
 	}
 }
