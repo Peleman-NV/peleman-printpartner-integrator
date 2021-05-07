6ca47dd4954f9feb19136adef4195f356e89f09b
Jason Goossens <jason.goossens@gmail.com>
2021-05-07 16:40:40 +0200

    added imaxel files to order object

--- API/PpiAPI.php
+++ API/PpiAPI.php
@@ -61,12 +61,11 @@ class PpiAPI
 	/**	
 	 * Register get processing orders endpoint
 	 */
-	public function registerGetProcessingOrderEndpoint()
+	public function registerGetProcessingOrdersEndpoint()
 	{
 		register_rest_route('ppi/v1', '/ordersprocessing', array(
 			'methods' => 'GET',
 			'callback' => array($this, 'getProcessingOrders'),
-			'args' => array('page'),
 			'permission_callback' => '__return_true'
 		));
 	}
@@ -74,8 +73,62 @@ class PpiAPI
 	public function getProcessingOrders()
 	{
 		global $wpdb;
-		$orderStatusProcessing = $wpdb->get_results('SELECT ID from ' . $wpdb->prefix . 'posts WHERE post_type = \'shop_order\' AND post_status = \'wc-processing\';');
+		$ordersWithStatusProcessing = $wpdb->get_results('SELECT ID from ' . $wpdb->prefix . 'posts WHERE post_type = \'shop_order\' AND post_status = \'wc-processing\';');
 
-		wp_send_json($orderStatusProcessing);
+		wp_send_json($ordersWithStatusProcessing);
+	}
+
+	/**	
+	 * Register custom get orders endpoint
+	 */
+	public function registerGetOrderEndpoint()
+	{
+		register_rest_route('ppi/v1', '/order(?:/(?P<order>\d+))?', array(
+			'methods' => 'GET',
+			'callback' => array($this, 'getOrder'),
+			'args' => array('order'),
+			'permission_callback' => '__return_true'
+		));
+	}
+
+	public function getOrder($request)
+	{
+		$orderId = $request['order'];
+
+		$order = wc_get_order($orderId);
+
+		if ($order === false) wp_send_json(['error' => "No order found for id {$orderId}"], 404);
+
+		$orderItems = $order->get_items();
+
+		$imaxel_files = [];
+		foreach ($orderItems as $orderItem) {
+			// assuming the content will have an Imaxel project ID (ie. has some customer created content).
+			$imaxelProjectId = $orderItem->get_meta('ppi_imaxel_project_id');
+			if ($imaxelProjectId === '') continue;
+			$fileName = "{$imaxelProjectId}/{$orderId}-{$imaxelProjectId}.zip";
+			$isFileReady = is_file(realpath(PPI_IMAXEL_FILES_DIR . '/' . $fileName));
+
+			if ($isFileReady === false) wp_send_json(['error' => "Files not ready for order id {$orderId}"], 404);
+
+			$imaxel_files[$imaxelProjectId] = get_site_url() . "/wp-content/uploads/ppi/imaxelfiles/{$fileName}";
+		}
+
+		try {
+			$api = $this->apiClient();
+			$endpoint = 'orders/' . $orderId;
+			$orderObject = $api->get($endpoint);
+
+			foreach ($orderObject->line_items as $lineItem) {
+				foreach ($lineItem->meta_data as $meta_data) {
+					if ($meta_data->key === 'ppi_imaxel_project_id') {
+						$lineItem->imaxel_files = $imaxel_files[$meta_data->value];
+					}
+				}
+			}
+			wp_send_json([$orderObject], 200);
+		} catch (\Throwable $th) {
+			wp_send_json(['error' => "Error adding files to response for order id {$orderId}"], 404);
+		}
 	}
 }
