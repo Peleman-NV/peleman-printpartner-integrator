569a222c7e3358aa234796fa59b41a691de94fe3
Jason Goossens <jason.goossens@gmail.com>
2021-05-11 14:42:51 +0200

    refactored order endpoints into one

--- Includes/Plugin.php
+++ Includes/Plugin.php
@@ -163,7 +163,6 @@ class Plugin
 	{
 		$plugin_api = new PpiAPI($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action('rest_api_init', $plugin_api, 'registerGetProcessingOrdersEndpoint');
 		$this->loader->add_action('rest_api_init', $plugin_api, 'registerGetOrderEndpoint');
 	}
 
